4eb62cc5af8b2b1b0b109cd508c1b9d1025c2115
malivaibhav15 <pranali.lembhe77@gmail.com>
2020-03-14 17:16:42 +0530

    ADDED : Test cases for maximum float value

--- src/test/java/FindMaximumTestCases.java
+++ src/test/java/FindMaximumTestCases.java
@@ -25,5 +25,26 @@ public class FindMaximumTestCases
         Assert.assertEquals(30,(int)maxNumber);
     }
 
+    @Test
+    public void givenFloat_AtPositionOne_ShouldReturnMaximum()
+    {
+        Generics findMaximum = new Generics();
+        Float maxNumber = findMaximum.maximumNumber(30.3f,10.01f,20.22f);
+        Assert.assertEquals((Float) 30.3f,maxNumber);
+    }
+    @Test
+    public void givenFloat_TwoIntegersSame_ShouldReturnMaximum()
+    {
+        Generics findMaximum = new Generics();
+        Float maxNumber = findMaximum.maximumNumber(10.1f,30.20f,30.20f);
+        Assert.assertEquals((Float) 30.20f,maxNumber);
+    }
+    @Test
+    public void givenFloat_ThreeIntegerssame_ShouldReturnMaximum()
+    {
+        Generics findMaximum = new Generics();
+        Float maxNumber = findMaximum.maximumNumber(30.10f,30.10f,30.10f);
+        Assert.assertEquals((Float)30.10f,maxNumber);
+    }
 
 }
