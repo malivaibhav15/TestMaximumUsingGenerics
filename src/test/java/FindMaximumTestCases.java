d1f5176556286daf9d62a0f75e47bb7bd4aebf87
malivaibhav15 <pranali.lembhe77@gmail.com>
2020-03-14 16:48:20 +0530

    ADDED: Test cases to return maximum number

--- src/test/java/FindMaximumTestCases.java
+++ src/test/java/FindMaximumTestCases.java
@@ -1,4 +1,29 @@
-package PACKAGE_NAME;
+import org.junit.Assert;
+import org.junit.Test;
 
-public class FindMaximumTestCases {
+public class FindMaximumTestCases
+{
+    @Test
+    public void givenIntegers_AtPositionOne_ShouldReturnMaximum()
+    {
+        Generics findMaximum = new Generics();
+        Integer maxNumber = findMaximum.maximumNumber(30,10,20);
+        Assert.assertEquals(30,(int)maxNumber);
+    }
+    @Test
+    public void givenIntegers_TwoIntegersSame_ShouldReturnMaximum()
+    {
+        Generics findMaximum = new Generics();
+        Integer maxNumber = findMaximum.maximumNumber(10,30,30);
+        Assert.assertEquals(30,(int)maxNumber);
+    }
+    @Test
+    public void givenIntegers_ThreeIntegerssame_ShouldReturnMaximum()
+    {
+        Generics findMaximum = new Generics();
+        Integer maxNumber = findMaximum.maximumNumber(30,30,30);
+        Assert.assertEquals(30,(int)maxNumber);
+    }
+
+
 }
