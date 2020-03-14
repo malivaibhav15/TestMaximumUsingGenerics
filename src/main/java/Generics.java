4f8ed4c292122dc82401fb8b3c830315d4a71607
malivaibhav15 <pranali.lembhe77@gmail.com>
2020-03-14 18:01:25 +0530

    ADDED : Method to find maximum

--- src/main/java/Generics.java
+++ src/main/java/Generics.java
@@ -2,12 +2,11 @@ public class Generics
 {
     public static int maximumNumber(Integer number1 , Integer number2 , Integer number3)
     {
-        Integer maximum = Math.max(number1,number2);
-        if (maximum > number3)
+        Integer maximum = Math.max(number1, number2);
+        if (maximum >  number3)
             return maximum;
         else
             return number3;
-
     }
     public static float maximumNumber(Float number1 , Float number2 , Float number3)
     {
@@ -16,6 +15,13 @@ public class Generics
             return maximum;
         else
             return number3;
-
+    }
+    public static String largestString(String first, String second, String third) {
+        String large = first;
+        if (second.compareTo(large) > 0)
+            large = second;
+        if (third.compareTo(large) > 0)
+            large = third;
+        return large;
     }
 }
