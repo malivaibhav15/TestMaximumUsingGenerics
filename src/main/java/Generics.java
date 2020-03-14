fe489dfe0ac34907d6b7bd3454adab7984e0f8a1
malivaibhav15 <pranali.lembhe77@gmail.com>
2020-03-14 18:29:03 +0530

    REFACTORED : Use generic method

--- src/main/java/Generics.java
+++ src/main/java/Generics.java
@@ -1,27 +1,14 @@
 public class Generics
 {
-    public static int maximumNumber(Integer number1 , Integer number2 , Integer number3)
-    {
-        Integer maximum = Math.max(number1, number2);
-        if (maximum >  number3)
-            return maximum;
-        else
-            return number3;
-    }
-    public static float maximumNumber(Float number1 , Float number2 , Float number3)
-    {
-        Float maximum = Math.max(number1,number2);
-        if (maximum > number3)
-            return maximum;
-        else
-            return number3;
-    }
-    public static String largestString(String first, String second, String third) {
-        String large = first;
-        if (second.compareTo(large) > 0)
-            large = second;
-        if (third.compareTo(large) > 0)
-            large = third;
-        return large;
+    public static <E extends Comparable> E checkMaximum(E first, E second, E third) {
+
+        E maximum = first;
+
+        if (second.compareTo(maximum) > 0)
+            maximum = second;
+        if (third.compareTo(maximum) > 0)
+            maximum = third;
+
+        return maximum;
     }
 }
