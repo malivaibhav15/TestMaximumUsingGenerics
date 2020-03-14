1f730683be50c564254eeeaad6da1fc2cce57058
malivaibhav15 <pranali.lembhe77@gmail.com>
2020-03-14 19:34:11 +0530

    REFACTOR : Test cases for generic class

--- src/main/java/Generics.java
+++ src/main/java/Generics.java
@@ -1,14 +1,33 @@
-public class Generics
-{
-    public static <E extends Comparable> E checkMaximum(E first, E second, E third) {
+public class Generics <E extends Comparable> {
+    E firstValue;
+    E secondValue;
+    E thirdValue;
 
-        E maximum = first;
+    //PARAMETERISED CONSTRUCTOR
+    public Generics(E firstValue, E secondValue, E thirdValue) {
+        this.firstValue = firstValue;
+        this.secondValue = secondValue;
+        this.thirdValue = thirdValue;
+    }
 
-        if (second.compareTo(maximum) > 0)
-            maximum = second;
-        if (third.compareTo(maximum) > 0)
-            maximum = third;
+    public Generics() {
 
-        return maximum;
+    }
+
+    //METHOD TO COMPARE GENERIC PARAMETERS AND RETURN MAXIMUM
+
+    public <E extends Comparable> E testMaximum(E firstValue, E secondValue, E thirdValue) {
+        if (firstValue.compareTo(secondValue) >= 0 && firstValue.compareTo(thirdValue) >= 0) {
+            return (E) firstValue;
+        } else if (secondValue.compareTo(firstValue) >= 0 && secondValue.compareTo(thirdValue) >= 0) {
+            return (E) secondValue;
+        } else {
+            return (E) thirdValue;
+        }
+    }
+    //METHOD OVERRIDE TO INITIALISE THE PARAMETERS
+    public E testMaximum ()
+    {
+        return testMaximum(firstValue, secondValue, thirdValue);
     }
 }
